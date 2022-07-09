cd301e447c529cbb5ff0b8ade2b959201de40469
arslonga83 <jlwood83@gmail.com>
2022-07-09 08:32:46 -0700

    single numbers print on screen.

--- script.js
+++ script.js
@@ -26,3 +26,57 @@ function operate (a, operator, b) {
     }
 }
 
+const display = document.querySelector('.display');
+const btn1 = document.querySelector('#one');
+const btn2 = document.querySelector('#two');
+const btn3 = document.querySelector('#three');
+const btn4 = document.querySelector('#four');
+const btn5 = document.querySelector('#five');
+const btn6 = document.querySelector('#six');
+const btn7 = document.querySelector('#seven');
+const btn8 = document.querySelector('#eight');
+const btn9 = document.querySelector('#nine');
+const btn0 = document.querySelector('#zero');
+
+
+btn1.addEventListener('click', () => {
+    display.textContent = '1';
+})
+
+btn2.addEventListener('click', () => {
+    display.textContent = '2';
+})
+
+btn3.addEventListener('click', () => {
+    display.textContent = '3';
+})
+
+btn4.addEventListener('click', () => {
+    display.textContent = '4';
+})
+
+btn5.addEventListener('click', () => {
+    display.textContent = '5';
+})
+
+btn6.addEventListener('click', () => {
+    display.textContent = '6';
+})
+
+btn7.addEventListener('click', () => {
+    display.textContent = '7';
+})
+
+btn8.addEventListener('click', () => {
+    display.textContent = '8';
+})
+
+btn9.addEventListener('click', () => {
+    display.textContent = '9';
+})
+
+btn0.addEventListener('click', () => {
+    display.textContent = '0';
+})
+
+
