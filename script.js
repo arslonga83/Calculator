2afce0de245e75fd311ce7a2743af729236e080f
arslonga83 <jlwood83@gmail.com>
2022-07-09 22:40:13 -0700

    update eventlistener code to allow multiple numbers by taking memory values from the display

--- script.js
+++ script.js
@@ -25,52 +25,29 @@ function operate ([a, operator, b]) {
         return total = divide (a, b);
     }
 }
+
 const nums = document.querySelectorAll('.num');
 const symbols = document.querySelectorAll('.symbol');
 const display = document.querySelector('.display');
 const btnequals = document.querySelector('#equals');
 const btnclear = document.querySelector('#clear');
 
-//we'll use these to store the totals as we go
 let memory = '';
 let formula = [];
 
-//uses the text printed on each button for both the display and the memory
-//nums.forEach(item => {
-//    item.addEventListener('click', () => {
-//    display.textContent += item.textContent;
-//    memory += item.textContent
-//    })
-//})
-
-//symbols.forEach(item => {
-//    item.addEventListener('click', () => {
-//    display.textContent = item.textContent;
-//    formula.push(memory, item.textContent);
-//    memory = '';
-//    })
-//})
-
-btnequals.addEventListener('click', () => {
-    formula.push(memory);
-    formula.join();
-    display.textContent = operate(formula);
-    memory = '';
-    formula = [];
-});
-
-btnclear.addEventListener('click', () => {
-    display.textContent = '';
-    memory = '';
-    formula = [];
+nums.forEach(item => {
+    item.addEventListener('click', () => {
+    if (formula.length === 2 || memory === 'reset') {
+        display.textContent = '';
+    }
+    display.textContent += item.textContent;
+    memory = ''
+    })
 })
 
-
-
-
-//FIGURE THIS OUT LATER
 symbols.forEach(item => {
     item.addEventListener('click', () => {
+    memory = display.textContent
     display.textContent = item.textContent;
     formula.push(memory);
     if (formula.length === 3) {
@@ -78,25 +55,28 @@ symbols.forEach(item => {
         display.textContent = operate(formula);
         memory = display.textContent;
         formula = [display.textContent, item.textContent];
-        console.log(formula)
-        console.log(memory)
     }
     else {
         formula.push(item.textContent)
         memory = ''
-        console.log(memory)
     }
     })
 })
 
-nums.forEach(item => {
-    item.addEventListener('click', () => {
-    if (formula.length === 2) {
-        display.textContent = '';
-        console.log(memory)
-    }
-    display.textContent += item.textContent;
-    memory = +item.textContent + +memory;
-    console.log(memory)
-    })
+btnequals.addEventListener('click', () => {
+    formula.push(display.textContent);
+    formula.join();
+    display.textContent = operate(formula);
+    memory = 'reset';
+    formula = [];
+});
+
+btnclear.addEventListener('click', () => {
+    display.textContent = '';
+    memory = 'reset';
+    formula = [];
 })
+
+
+
+
