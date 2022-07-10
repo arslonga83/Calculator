3be8437c59c9dddde3dfeb9ded2f8510050b5d9a
arslonga83 <jlwood83@gmail.com>
2022-07-09 20:46:01 -0700

    group buttons by class to add event listeners. delete superflous code for single buttons.

--- script.js
+++ script.js
@@ -25,113 +25,30 @@ function operate ([a, operator, b]) {
         return total = divide (a, b);
     }
 }
-
+const nums = document.querySelectorAll('.num');
+const symbols = document.querySelectorAll('.symbol');
 const display = document.querySelector('.display');
-const btn1 = document.querySelector('#one');
-const btn2 = document.querySelector('#two');
-const btn3 = document.querySelector('#three');
-const btn4 = document.querySelector('#four');
-const btn5 = document.querySelector('#five');
-const btn6 = document.querySelector('#six');
-const btn7 = document.querySelector('#seven');
-const btn8 = document.querySelector('#eight');
-const btn9 = document.querySelector('#nine');
-const btn0 = document.querySelector('#zero');
-const btnplus = document.querySelector('#plus');
-const btnminus = document.querySelector('#minus');
-const btntimes = document.querySelector('#times');
-const btndivide = document.querySelector('#divide');
 const btnequals = document.querySelector('#equals');
 const btnclear = document.querySelector('#clear');
 
+//we'll use these to store the totals as we go
 let memory = '';
 let formula = [];
 
-btn1.addEventListener('click', () => {
-    display.textContent += '1';
-    memory += '1';
+//uses the text printed on each button for both the display and the memory
+nums.forEach(item => {
+    item.addEventListener('click', () => {
+    display.textContent += item.textContent;
+    memory += item.textContent
+    })
 })
 
-btn2.addEventListener('click', () => {
-    display.textContent += '2';
-    memory += '2';
-})
-
-btn3.addEventListener('click', () => {
-    display.textContent += '3';
-    memory += '3';
-})
-
-btn4.addEventListener('click', () => {
-    display.textContent += '4';
-    memory += '4';
-})
-
-btn5.addEventListener('click', () => {
-    display.textContent += '5';
-    memory += '5';
-})
-
-btn6.addEventListener('click', () => {
-    display.textContent += '6';
-    memory += '6';
-})
-
-btn7.addEventListener('click', () => {
-    display.textContent += '7';
-    memory += '7';
-})
-
-btn8.addEventListener('click', () => {
-    display.textContent += '8';
-    memory += '8';
-})
-
-btn9.addEventListener('click', () => {
-    display.textContent += '9';
-    memory += '9';
-})
-
-btn0.addEventListener('click', () => {
-    display.textContent += '0';
-    memory += '0';
-})
-
-btnplus.addEventListener('click', () => {
-    display.textContent = '+';
-    formula.push(memory, '+');
+symbols.forEach(item => {
+    item.addEventListener('click', () => {
+    display.textContent = item.textContent;
+    formula.push(memory, item.textContent);
     memory = '';
-})
-
-   // formula.push(memory)
-   // if (formula.length === 3) {
-   //     formula.join();
-   //     memory = operate(formula);
-   //     display.textContent = memory;
-   // }
-   // else {
-   //     display.textContent = '+';
-   //     formula.push('+');
-   //     memory = '';
-   // }
-//})
-
-btnminus.addEventListener('click', () => {
-    display.textContent = '-';
-    formula.push(memory,'-');
-    memory = '';
-})
-
-btntimes.addEventListener('click', () => {
-    display.textContent = '*';
-    formula.push(memory, '*');
-    memory = '';
-})
-
-btndivide.addEventListener('click', () => {
-    display.textContent = '/';
-    formula.push(memory, '/');
-    memory = '';
+    })
 })
 
 btnequals.addEventListener('click', () => {
@@ -142,13 +59,9 @@ btnequals.addEventListener('click', () => {
     formula = [];
 });
 
-
 btnclear.addEventListener('click', () => {
     display.textContent = '';
     memory = '';
     formula = [];
 })
-
-
-
 
