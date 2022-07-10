9b8c500e0c6aa6d01b8281cfcec21071827bca8b
arslonga83 <jlwood83@gmail.com>
2022-07-09 21:25:37 -0700

    add test code to operate multiple numbers

--- script.js
+++ script.js
@@ -36,20 +36,20 @@ let memory = '';
 let formula = [];
 
 //uses the text printed on each button for both the display and the memory
-nums.forEach(item => {
-    item.addEventListener('click', () => {
-    display.textContent += item.textContent;
-    memory += item.textContent
-    })
-})
+//nums.forEach(item => {
+//    item.addEventListener('click', () => {
+//    display.textContent += item.textContent;
+//    memory += item.textContent
+//    })
+//})
 
-symbols.forEach(item => {
-    item.addEventListener('click', () => {
-    display.textContent = item.textContent;
-    formula.push(memory, item.textContent);
-    memory = '';
-    })
-})
+//symbols.forEach(item => {
+//    item.addEventListener('click', () => {
+//    display.textContent = item.textContent;
+//    formula.push(memory, item.textContent);
+//    memory = '';
+//    })
+//})
 
 btnequals.addEventListener('click', () => {
     formula.push(memory);
@@ -65,3 +65,38 @@ btnclear.addEventListener('click', () => {
     formula = [];
 })
 
+
+
+
+//FIGURE THIS OUT LATER
+symbols.forEach(item => {
+    item.addEventListener('click', () => {
+    display.textContent = item.textContent;
+    formula.push(memory);
+    if (formula.length === 3) {
+        formula.join();
+        display.textContent = operate(formula);
+        memory = display.textContent;
+        formula = [display.textContent, item.textContent];
+        console.log(formula)
+        console.log(memory)
+    }
+    else {
+        formula.push(item.textContent)
+        memory = ''
+        console.log(memory)
+    }
+    })
+})
+
+nums.forEach(item => {
+    item.addEventListener('click', () => {
+    if (formula.length === 2) {
+        display.textContent = '';
+        console.log(memory)
+    }
+    display.textContent += item.textContent;
+    memory = +item.textContent + +memory;
+    console.log(memory)
+    })
+})
